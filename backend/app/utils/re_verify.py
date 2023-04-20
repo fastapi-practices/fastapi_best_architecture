2802fc2253fb01d47c615353850f4f5dc7958b6f
wu <jianhengwu0407@gmail.com>
2023-04-20 16:07:02 +0800

    clean up the utils code

--- backend/app/utils/re_verify.py
+++ backend/app/utils/re_verify.py
@@ -41,23 +41,3 @@ def is_mobile(text: str) -> bool:
     :return:
     """
     return match_string(r"^1[3-9]\d{9}$", text)
-
-
-def is_wechat(text: str) -> bool:
-    """
-    检查微信号
-
-    :param text:
-    :return:
-    """
-    return match_string(r"^[a-zA-Z]([-_a-zA-Z0-9]{5,19})+$", text)
-
-
-def is_qq(text: str) -> bool:
-    """
-    检查QQ号
-
-    :param text:
-    :return:
-    """
-    return match_string(r"^[1-9][0-9]{4,10}$", text)
