4e637c407c44d4c54026609c8f67d8287d57cb14
wu <jianhengwu0407@gmail.com>
2023-04-21 23:58:00 +0800

    update the location and comments of the config file constants

--- backend/app/core/conf.py
+++ backend/app/core/conf.py
@@ -7,10 +7,37 @@ from pydantic import BaseSettings, root_validator
 
 
 class Settings(BaseSettings):
+    # Env Config
     ENVIRONMENT: str
+
+    # Env MySQL
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+
+    # Env Redis
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASSWORD: str
+    REDIS_DATABASE: int
+
+    # Env APScheduler Redis
+    APS_REDIS_HOST: str
+    APS_REDIS_PORT: int
+    APS_REDIS_PASSWORD: str
+    APS_REDIS_DATABASE: int
+
+    # Env Token
+    TOKEN_SECRET_KEY: str  # 密钥 secrets.token_urlsafe(32))
+
+    # Env Email
+    EMAIL_USER: str
+    EMAIL_PASSWORD: str  # 授权密码，非邮箱密码
+
     # FastAPI
     TITLE: str = 'FastAPI'
-    VERSION: str = 'v0.0.1'
+    VERSION: str = '0.0.1'
     DESCRIPTION: str = "FastAPI Best Architecture"
     DOCS_URL: Optional[str] = '/v1/docs'
     REDOCS_URL: Optional[str] = '/v1/redocs'
@@ -32,25 +59,13 @@ class Settings(BaseSettings):
 
     # MySQL
     DB_ECHO: bool = False
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASSWORD: str
     DB_DATABASE: str = 'fba'
     DB_CHARSET: str = 'utf8mb4'
 
     # Redis
-    REDIS_HOST: str
-    REDIS_PORT: int
-    REDIS_PASSWORD: str
-    REDIS_DATABASE: int
     REDIS_TIMEOUT: int = 5
 
-    # APScheduler DB
-    APS_REDIS_HOST: str
-    APS_REDIS_PORT: int
-    APS_REDIS_PASSWORD: str
-    APS_REDIS_DATABASE: int = 1
+    # APScheduler Redis
     APS_REDIS_TIMEOUT: int = 10
 
     # APScheduler Default
@@ -60,7 +75,6 @@ class Settings(BaseSettings):
 
     # Token
     TOKEN_ALGORITHM: str = 'HS256'  # 算法
-    TOKEN_SECRET_KEY: str  # 密钥 secrets.token_urlsafe(32))
     TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 1  # token 时效 60 * 24 * 1 = 1 天
     TOKEN_URL: str = '/v1/users/login'
 
@@ -68,12 +82,7 @@ class Settings(BaseSettings):
     EMAIL_DESCRIPTION: str = 'fastapi_sqlalchemy_mysql'  # 默认发件说明
     EMAIL_SERVER: str = 'smtp.qq.com'
     EMAIL_PORT: int = 465
-    EMAIL_USER: str
-    EMAIL_PASSWORD: str  # 授权密码，非邮箱密码
-    EMAIL_SSL: bool = True  # 是否使用ssl
-
-    # 邮箱登录验证码过期时间
-    EMAIL_LOGIN_CODE_MAX_AGE: int = 60 * 2  # 时效 60 * 2 = 2 分钟
+    EMAIL_SSL: bool = True
 
     # Cookies
     COOKIES_MAX_AGE: int = 60 * 5  # cookies 时效 60 * 5 = 5 分钟
