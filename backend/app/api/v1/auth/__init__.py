2a51c76493d78ab65f1aaca7faa1c5706a891aa3
Wu Clan <jianhengwu0407@gmail.com>
2023-05-07 15:21:33 +0800

    Update the sub router settings in the subdirectory (#32)

--- backend/app/api/v1/auth/__init__.py
+++ backend/app/api/v1/auth/__init__.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from backend.app.api.v1.auth.user import router
+from fastapi import APIRouter
+from backend.app.api.v1.auth.user import router as user_router
+
+router = APIRouter(prefix='/auth', tags=['用户管理'])
+
+router.include_router(user_router, prefix='/users')
