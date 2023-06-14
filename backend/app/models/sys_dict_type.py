f4882b644416c5d1db2da7f8aaa30d288bd0eacf
Wu Clan <jianhengwu0407@gmail.com>
2023-06-14 15:37:30 +0800

    Update sys_dict_type.py
    fix relationship field annotation

--- backend/app/models/sys_dict_type.py
+++ backend/app/models/sys_dict_type.py
@@ -17,5 +17,5 @@ class DictType(Base):
     code: Mapped[str] = mapped_column(String(32), unique=True, comment='字典类型编码')
     status: Mapped[bool] = mapped_column(default=True, comment='状态（0停用 1正常）')
     remark: Mapped[str | None] = mapped_column(LONGTEXT, default=None, comment='备注')
-    # 字典数据一对多
+    # 字典类型一对多
     datas: Mapped[list['DictData']] = relationship(init=False, back_populates='type')  # noqa: F821
