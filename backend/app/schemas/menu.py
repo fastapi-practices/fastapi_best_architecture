846f714fb46e976b0c3747e0d62db2d22ac07cd4
Wu Clan <jianhengwu0407@gmail.com>
2023-07-01 19:04:44 +0800

    Update menu table fields

--- backend/app/schemas/menu.py
+++ backend/app/schemas/menu.py
@@ -8,19 +8,6 @@ from backend.app.common.enums import MenuType, StatusType
 from backend.app.schemas.base import SchemaBase
 
 
-class Meta(SchemaBase):
-    roles: list[str] | None = None
-    requiresAuth: bool = False
-    icon: str | None = None
-    locale: str | None = None
-    hideInMenu: bool | None = None
-    hideChildrenInMenu: bool | None = None
-    activeMenu: str | None = None
-    orderNo: int | None = None
-    noAffix: bool | None = None
-    ignoreCache: bool | None = None
-
-
 class MenuBase(SchemaBase):
     name: str
     parent_id: int | None = Field(default=None, description='菜单父级ID')
@@ -28,10 +15,11 @@ class MenuBase(SchemaBase):
     icon: str | None = None
     path: str | None = None
     menu_type: MenuType = Field(default=MenuType.directory, ge=0, description='菜单类型（0目录 1菜单 2按钮）')
-    meta: Meta
     component: str | None = None
     perms: str | None = None
     status: StatusType = Field(default=StatusType.enable)
+    show: StatusType = Field(default=StatusType.enable)
+    cache: StatusType = Field(default=StatusType.enable)
     remark: str | None = None
 
 
