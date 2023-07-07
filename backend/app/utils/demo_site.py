16cd531ea7be8e509ba22887184b398566365a12
Wu Clan <jianhengwu0407@gmail.com>
2023-07-07 17:49:57 +0800

    fix demo site whitelist

--- backend/app/utils/demo_site.py
+++ backend/app/utils/demo_site.py
@@ -11,5 +11,10 @@ async def demo_site(request: Request):
 
     method = request.method
     path = request.url.path
-    if settings.DEMO_MODE and method != 'GET' and method != 'OPTIONS' and (method, path) not in settings.CASBIN_EXCLUDE:
+    if (
+        settings.DEMO_MODE
+        and method != 'GET'
+        and method != 'OPTIONS'
+        and (method, path) not in settings.DEMO_MODE_EXCLUDE
+    ):
         raise errors.ForbiddenError(msg='演示环境下禁止执行此操作')
