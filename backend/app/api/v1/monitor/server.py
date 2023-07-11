9134b52e2058fae828524f3a2a0aeb4ae825fad1
Wu Clan <jianhengwu0407@gmail.com>
2023-07-12 03:06:46 +0800

    Update monitoring return data

--- backend/app/api/v1/monitor/server.py
+++ backend/app/api/v1/monitor/server.py
@@ -5,19 +5,19 @@ from starlette.concurrency import run_in_threadpool
 
 from backend.app.common.jwt import DependsJwtAuth
 from backend.app.common.response.response_schema import response_base
-from backend.app.utils.server_info import ServerInfo
+from backend.app.utils.server_info import server_info
 
 router = APIRouter()
 
 
 @router.get('/server', summary='server 监控', dependencies=[DependsJwtAuth])
-async def server_info():
+async def get_server_info():
     """IO密集型任务，使用线程池尽量减少性能损耗"""
     data = {
-        'cpu': await run_in_threadpool(ServerInfo.get_cpu_info),
-        'mem': await run_in_threadpool(ServerInfo.get_mem_info),
-        'sys': await run_in_threadpool(ServerInfo.get_sys_info),
-        'disk': await run_in_threadpool(ServerInfo.get_disk_info),
-        'service': await run_in_threadpool(ServerInfo.get_service_info),
+        'cpu': await run_in_threadpool(server_info.get_cpu_info),
+        'mem': await run_in_threadpool(server_info.get_mem_info),
+        'sys': await run_in_threadpool(server_info.get_sys_info),
+        'disk': await run_in_threadpool(server_info.get_disk_info),
+        'service': await run_in_threadpool(server_info.get_service_info),
     }
     return await response_base.success(data=data)
