d877c866642181bd86c57d5cc62c2a5c77106912
Wu Clan <jianhengwu0407@gmail.com>
2023-07-13 17:44:07 +0800

    Update monitoring return data (#176)

--- backend/app/api/v1/monitor/redis.py
+++ backend/app/api/v1/monitor/redis.py
@@ -3,25 +3,13 @@
 from fastapi import APIRouter
 
 from backend.app.common.jwt import DependsJwtAuth
-from backend.app.common.redis import redis_client
 from backend.app.common.response.response_schema import response_base
+from backend.app.utils.redis_info import redis_info
 
 router = APIRouter()
 
 
 @router.get('/redis', summary='redis 监控', dependencies=[DependsJwtAuth])
-async def redis_info():
-    info = await redis_client.info()
-    fmt_info = {}
-    for key, value in info.items():
-        if isinstance(value, dict):
-            value = ','.join({f'{k}={v}' for k, v in value.items()})
-        else:
-            value = str(value)
-        fmt_info[key] = value
-    db_size = await redis_client.dbsize()
-    command_stats = await redis_client.info('commandstats')
-    stats_list = []
-    for k, v in command_stats.items():
-        stats_list.append({'name': k.split('_')[-1], 'value': str(v.get('calls', ''))})
-    return await response_base.success(data={'info': fmt_info, 'stats': stats_list, 'size': db_size})
+async def get_redis_info():
+    data = {'info': await redis_info.get_info(), 'stats': await redis_info.get_stats()}
+    return await response_base.success(data=data)
