bdf88a4e6be5dc20bbf23413449191827d534e54
Wu Clan <jianhengwu0407@gmail.com>
2023-07-23 20:13:03 +0800

    Fix schema enum condition exception

--- backend/app/schemas/menu.py
+++ backend/app/schemas/menu.py
@@ -15,7 +15,7 @@ class MenuBase(SchemaBase):
     sort: int = Field(default=0, ge=0, description='排序')
     icon: str | None = None
     path: str | None = None
-    menu_type: MenuType = Field(default=MenuType.directory, ge=0, description='菜单类型（0目录 1菜单 2按钮）')
+    menu_type: MenuType = Field(default=MenuType.directory, description='菜单类型（0目录 1菜单 2按钮）')
     component: str | None = None
     perms: str | None = None
     status: StatusType = Field(default=StatusType.enable)
