679b8ff66640269308a0cfe86c52d74042d0b9fe
Wu Clan <jianhengwu0407@gmail.com>
2023-09-01 20:05:41 +0800

    Fix int enum class inheritance

--- backend/app/common/enums.py
+++ backend/app/common/enums.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from enum import Enum
+from enum import Enum, IntEnum as SourceIntEnum
+from typing import Type
 
 
-class EnumBase(Enum):
+class _EnumBase:
     @classmethod
-    def get_member_values(cls):
-        return [item.value for item in cls._member_map_.values()]
+    def get_member_keys(cls: Type[Enum]) -> list[str]:
+        return [name for name in cls.__members__.keys()]
 
     @classmethod
-    def get_member_names(cls):
-        return [name for name in cls._member_names_]
+    def get_member_values(cls: Type[Enum]) -> list:
+        return [item.value for item in cls.__members__.values()]
 
 
-class IntEnum(int, EnumBase):
+class IntEnum(_EnumBase, SourceIntEnum):
     """整型枚举"""
 
     pass
 
 
-class StrEnum(str, EnumBase):
+class StrEnum(_EnumBase, str, Enum):
     """字符串枚举"""
 
     pass
