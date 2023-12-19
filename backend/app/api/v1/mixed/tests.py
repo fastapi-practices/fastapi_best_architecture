4daeb8efb55103050fa396c0248f2d0960fd5a7b
Wu Clan <jianhengwu0407@gmail.com>
2023-12-19 22:58:00 +0800

    Prepare to lock the pydantic-v1 branch

--- backend/app/api/v1/mixed/tests.py
+++ backend/app/api/v1/mixed/tests.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from fastapi import APIRouter, File, Form, UploadFile
+from fastapi import APIRouter
 
+from backend.app.common.response.response_schema import response_base
 from backend.app.tasks import task_demo_async
 
 router = APIRouter(prefix='/tests')
@@ -10,13 +11,10 @@ router = APIRouter(prefix='/tests')
 @router.post('/send', summary='测试异步任务')
 async def task_send():
     result = task_demo_async.delay()
-    return {'msg': 'Success', 'data': result.id}
+    return await response_base.success(data=result.id)
 
 
-@router.post('/files', summary='测试文件上传')
-async def create_file(file: bytes = File(), fileb: UploadFile = File(), token: str = Form()):
-    return {
-        'file_size': len(file),
-        'token': token,
-        'fileb_content_type': fileb.content_type,
-    }
+@router.post('/send', summary='异步任务演示')
+async def send_task():
+    result = task_demo_async.delay()
+    return await response_base.success(data=result.id)
