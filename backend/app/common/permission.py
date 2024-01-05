89975b7e72ae7e03fe6899f1d15fff3bf3dc9434
Wu Clan <jianhengwu0407@gmail.com>
2024-01-06 01:46:42 +0800

    Add request permission dependency description

--- backend/app/common/permission.py
+++ backend/app/common/permission.py
@@ -7,6 +7,13 @@ from backend.app.core.conf import settings
 
 
 class RequestPermission:
+    """
+    请求权限，仅用于角色菜单RBAC
+
+    Tip:
+        使用此请求权限时，需要将 `Depends(RBAC.rbac_verify)` 在 `Depends(RequestPermission('xxx'))` 之前设置，
+        这是因为 fastapi 当前版本的依赖导入顺序为逆向导入，意味着 RBAC 标识会在验证前被设置
+    """
     def __init__(self, value: str):
         self.value = value
 
