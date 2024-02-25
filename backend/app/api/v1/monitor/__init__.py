8af4336428266e3e3af88106e1fd1de90e13d00f
Wu Clan <jianhengwu0407@gmail.com>
2024-02-25 20:48:25 +0800

    Update interface file directory level (#295)
    * Update interface file directory level
    
    * fix ruff format

--- backend/app/api/v1/monitor/__init__.py
+++ backend/app/api/v1/monitor/__init__.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter
 from backend.app.api.v1.monitor.redis import router as redis_router
 from backend.app.api.v1.monitor.server import router as server_router
 
-router = APIRouter()
+router = APIRouter(prefix='/monitors', tags=['监控管理'])
 
 router.include_router(redis_router)
 router.include_router(server_router)
