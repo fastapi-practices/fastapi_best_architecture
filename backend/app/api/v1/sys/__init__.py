bd3843aef8c2107d9851829c13de0d86d3bed0bc
Wu Clan <jianhengwu0407@gmail.com>
2024-02-25 16:35:19 +0800

    fix ruff format

--- backend/app/api/v1/sys/__init__.py
+++ backend/app/api/v1/sys/__init__.py
@@ -13,11 +13,11 @@ from backend.app.api.v1.sys.user import router as user_router
 
 router = APIRouter(prefix='/sys')
 
-router.include_router(api_router, prefix="/apis", tags=['API管理'])
+router.include_router(api_router, prefix='/apis', tags=['API管理'])
 router.include_router(casbin_router, prefix='/casbin', tags=['Casbin权限管理'])
-router.include_router(dept_router, prefix="/depts", tags=['部门管理'])
-router.include_router(dict_data_router, prefix="/dict_datas", tags=['字典数据管理'])
-router.include_router(dict_type_router, prefix="/dict_types", tags=['字典类型管理'])
-router.include_router(menu_router, prefix="/menus", tags=['目录管理'])
-router.include_router(role_router, prefix="/roles", tags=['角色管理'])
-router.include_router(user_router, prefix="/users", tags=['用户管理'])
+router.include_router(dept_router, prefix='/depts', tags=['部门管理'])
+router.include_router(dict_data_router, prefix='/dict_datas', tags=['字典数据管理'])
+router.include_router(dict_type_router, prefix='/dict_types', tags=['字典类型管理'])
+router.include_router(menu_router, prefix='/menus', tags=['目录管理'])
+router.include_router(role_router, prefix='/roles', tags=['角色管理'])
+router.include_router(user_router, prefix='/users', tags=['用户管理'])
