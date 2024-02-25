df819d1681e81a8319a4b3fb3afaef84287c45bb
Wu Clan <jianhengwu0407@gmail.com>
2024-02-25 21:25:34 +0800

    Automatically redirect authorization addresses

--- backend/app/api/v1/auth/oauth2.py
+++ backend/app/api/v1/auth/oauth2.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter, Depends
 from fastapi_oauth20 import FastAPIOAuth20, GoogleOAuth20
+from starlette.responses import RedirectResponse
 
 from backend.app.core.conf import settings
 
@@ -11,14 +12,15 @@ google_client = GoogleOAuth20(settings.OAUTH2_GOOGLE_CLIENT_ID, settings.OAUTH2_
 google_oauth2 = FastAPIOAuth20(google_client, settings.OAUTH2_GOOGLE_REDIRECT_URI)
 
 
-@router.get('/google', summary='获取 Google 授权地址')
-async def auth_google() -> str:
-    return await google_client.get_authorization_url(redirect_uri=settings.OAUTH2_REDIRECT_URI)
+@router.get('/google', summary='Google 授权')
+async def auth_google() -> RedirectResponse:
+    auth_url = await google_client.get_authorization_url(redirect_uri=settings.OAUTH2_REDIRECT_URI)
+    return RedirectResponse(url=auth_url)
 
 
 @router.post(
     '/login/google',
-    summary='谷歌授权重定向地址',
+    summary='谷歌授权后重定向地址',
     description='Google 授权后，重定向到当前地址获取 Google 用户信息，通过用户信息自动创建系统用户',
 )
 async def login_google(oauth: google_oauth2 = Depends()):
