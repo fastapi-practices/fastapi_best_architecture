76b6d05f989b0ab926d1f99e27f0f68f06152967
Wu Clan <jianhengwu0407@gmail.com>
2024-05-17 12:34:13 +0800

    update template util

---
+++ backend/utils/gen_template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from jinja2 import Environment, FileSystemLoader, Template, select_autoescape
+
+from backend.core.path_conf import JINJA2_TEMPLATE_DIR
+
+
+class GenTemplate:
+
+    @staticmethod
+    def get_template(jinja_file: str) -> 'Template':
+        """
+        获取模版文件
+
+        :param jinja_file:
+        :return:
+        """
+        env = Environment(
+            loader=FileSystemLoader(JINJA2_TEMPLATE_DIR),
+            autoescape=select_autoescape(['html', 'xml', 'jinja']),
+            keep_trailing_newline=True,
+        )
+        return env.get_template(jinja_file)
