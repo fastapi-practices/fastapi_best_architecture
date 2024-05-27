2b33441b913138cefe576a557348f521d341d0e5
Wu Clan <jianhengwu0407@gmail.com>
2024-05-27 22:44:47 +0800

    Fix gen model table

--- backend/app/generator/model/gen_model.py
+++ backend/app/generator/model/gen_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from sqlalchemy import ForeignKey, String, UniqueConstraint
+from typing import Union
+
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.common.model import DataClassBase, id_key
@@ -10,13 +12,12 @@ class GenModel(DataClassBase):
     """代码生成模型表"""
 
     __tablename__ = 'sys_gen_model'
-    __table_args__ = UniqueConstraint('gen_business_id')
 
     id: Mapped[id_key] = mapped_column(init=False)
     name: Mapped[str] = mapped_column(String(50), comment='列名称')
     comment: Mapped[str | None] = mapped_column(String(255), default=None, comment='列描述')
     type: Mapped[str] = mapped_column(String(20), default='string', comment='列类型')
-    default: Mapped[str | None] = mapped_column(default=None, comment='列默认值')
+    default: Mapped[str | None] = mapped_column(String(50), default=None, comment='列默认值')
     sort: Mapped[int | None] = mapped_column(default=1, comment='列排序')
     length: Mapped[int] = mapped_column(default=0, comment='列长度')
     is_pk: Mapped[bool] = mapped_column(default=False, comment='是否主键')
@@ -24,6 +25,6 @@ class GenModel(DataClassBase):
 
     # 代码生成业务model一对多
     gen_business_id: Mapped[int | None] = mapped_column(
-        ForeignKey('sys_gen_business.id', default=None, comment='代码生成业务ID')
+        ForeignKey('sys_gen_business.id', ondelete='CASCADE'), default=None, comment='代码生成业务ID'
     )
     gen_business: Mapped[Union['GenBusiness', None]] = relationship(init=False, back_populates='gen_model')  # noqa: F821
