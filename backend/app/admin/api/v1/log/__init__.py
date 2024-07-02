f21af0c3c31660f7b055cc6bfc39f4d9446b9e10
Wu Clan <jianhengwu0407@gmail.com>
2024-07-02 09:29:22 +0800

    Update app route definition rules (#341)

--- backend/app/admin/api/v1/log/__init__.py
+++ backend/app/admin/api/v1/log/__init__.py
@@ -7,5 +7,5 @@ from backend.app.admin.api.v1.log.opera_log import router as opera_log
 
 router = APIRouter(prefix='/logs')
 
-router.include_router(login_log, prefix='/login', tags=['登录日志管理'])
-router.include_router(opera_log, prefix='/opera', tags=['操作日志管理'])
+router.include_router(login_log, prefix='/login', tags=['登录日志'])
+router.include_router(opera_log, prefix='/opera', tags=['操作日志'])
