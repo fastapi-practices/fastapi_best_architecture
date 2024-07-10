1b4b20ed3d957c8adf391ded6fa282b733b56e34
Wu Clan <jianhengwu0407@gmail.com>
2024-07-10 10:34:35 +0800

    Optimize schema name generation

--- backend/app/generator/schema/gen_business.py
+++ backend/app/generator/schema/gen_business.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 
 from pydantic import ConfigDict, Field, model_validator
+from pydantic.alias_generators import to_pascal
 
 from backend.common.schema import SchemaBase
 
@@ -24,7 +25,7 @@ class GenBusinessSchemaBase(SchemaBase):
     @model_validator(mode='after')
     def check_schema_name(self):
         if self.schema_name is None:
-            self.schema_name = self.table_name_en
+            self.schema_name = to_pascal(self.table_name_en)
         return self
 
 
