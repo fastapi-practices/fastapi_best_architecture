dd29c99b424a8ce4ac2d63f0c3bcd8a8b348c2d8
Wu Clan <jianhengwu0407@gmail.com>
2024-07-25 04:25:15 +0800

    fix config schema

--- backend/app/admin/schema/config.py
+++ backend/app/admin/schema/config.py
@@ -11,7 +11,7 @@ class ConfigSchemaBase(SchemaBase):
     login_title: str = Field(default='登陆 FBA')
     login_sub_title: str = Field(default='fastapi_best_architecture')
     footer: str = Field(default='FBA')
-    system_logo: str = Field(default='Arco')
+    logo: str = Field(default='Arco')
     system_title: str = Field(default='Arco')
     system_comment: str = Field(
         default='基于 FastAPI 构建的前后端分离 RBAC 权限控制系统，采用独特的伪三层架构模型设计，'
