39060346b22143f0e7fb9ea6e68458f4b9ae7792
Wu Clan <jianhengwu0407@gmail.com>
2024-07-26 18:56:38 +0800

    Fix celery asynchronous task execution

--- backend/app/task/celery_task/tasks.py
+++ backend/app/task/celery_task/tasks.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 import uuid
 
+from anyio import sleep
+
 from backend.app.task.celery import celery_app
 
 
 @celery_app.task(name='task_demo_async')
-def task_demo_async() -> str:
+async def task_demo_async() -> str:
+    await sleep(1)
     uid = uuid.uuid4().hex
     print(f'异步任务 {uid} 执行成功')
     return uid
