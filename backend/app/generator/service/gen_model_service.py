144d704a5d08bb46d009de1bb56ed0c7e0b2fd9e
Wu Clan <jianhengwu0407@gmail.com>
2024-08-11 21:32:09 +0800

    Fix code generation model create and update (#378)

--- backend/app/generator/service/gen_model_service.py
+++ backend/app/generator/service/gen_model_service.py
@@ -20,15 +20,15 @@ class GenModelService:
     @staticmethod
     async def get_by_business(*, business_id: int) -> Sequence[GenModel]:
         async with async_db_session() as db:
-            gen_model = await gen_model_dao.get_all_by_business_id(db, business_id)
-            return gen_model
+            gen_models = await gen_model_dao.get_all_by_business_id(db, business_id)
+            return gen_models
 
     @staticmethod
     async def create(*, obj: CreateGenModelParam) -> None:
         async with async_db_session.begin() as db:
-            gen_model = await gen_model_dao.get_by_name(db, obj.name)
-            if gen_model:
-                raise errors.ForbiddenError(msg='禁止添加相同列到模型表')
+            gen_models = await gen_model_dao.get_all_by_business_id(db, obj.gen_business_id)
+            if obj.name in [gen_model.name for gen_model in gen_models]:
+                raise errors.ForbiddenError(msg='禁止添加相同列到同一模型表')
             pd_type = sql_type_to_pydantic(obj.type)
             await gen_model_dao.create(db, obj, pd_type=pd_type)
 
@@ -37,9 +37,9 @@ class GenModelService:
         async with async_db_session.begin() as db:
             model = await gen_model_dao.get(db, pk)
             if obj.name != model.name:
-                model_check = await gen_model_dao.get_by_name(db, obj.name)
-                if model_check:
-                    raise errors.ForbiddenError(msg='禁止添加相同列到模型表')
+                gen_models = await gen_model_dao.get_all_by_business_id(db, obj.gen_business_id)
+                if obj.name in [gen_model.name for gen_model in gen_models]:
+                    raise errors.ForbiddenError(msg='模型列名已存在')
             pd_type = sql_type_to_pydantic(obj.type)
             count = await gen_model_dao.update(db, pk, obj, pd_type=pd_type)
             return count
