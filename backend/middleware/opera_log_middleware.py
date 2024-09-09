9c1768c9f85a02373ceddb724bdc88903c3e432e
Wu Clan <jianhengwu0407@gmail.com>
2024-09-09 12:01:20 +0800

    del todo

--- backend/middleware/opera_log_middleware.py
+++ backend/middleware/opera_log_middleware.py
@@ -101,8 +101,7 @@ class OperaLogMiddleware(BaseHTTPMiddleware):
         try:
             response = await call_next(request)
         except Exception as e:
-            # TODO: traceId
-            log.error(f'请求异常, {e}')
+            log.error(f'请求异常: {e}')
             code, msg = await self.request_exception_handler(request, code, msg)
             # code 处理包含 SQLAlchemy 和 Pydantic
             code = getattr(e, 'code', None) or code
