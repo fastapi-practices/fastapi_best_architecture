1da94fb9c1014eae3d482d7a66e89e616dbf2b18
Wu Clan <jianhengwu0407@gmail.com>
2024-11-12 19:49:47 +0800

    Update multiple version dependency specifiers

--- backend/app/admin/tests/api_v1/test_auth.py
+++ backend/app/admin/tests/api_v1/test_auth.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 from starlette.testclient import TestClient
 
-from backend.core.conf import settings
-
 
 def test_logout(client: TestClient, token_headers: dict[str, str]) -> None:
-    response = client.post(f'{settings.FASTAPI_API_V1_PATH}/auth/logout', headers=token_headers)
+    response = client.post('/auth/logout', headers=token_headers)
     assert response.status_code == 200
     assert response.json()['code'] == 200
--- backend/app/admin/tests/utils/db_mysql.py
+++ backend/app/admin/tests/utils/db_mysql.py
@@ -5,12 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from backend.core.conf import settings
 from backend.database.db_mysql import create_engine_and_session
 
-TEST_SQLALCHEMY_DATABASE_URL = (
+_, test_async_db_session = create_engine_and_session(
     f'mysql+asyncmy://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}:'
     f'{settings.MYSQL_PORT}/{settings.MYSQL_DATABASE}_test?charset={settings.MYSQL_CHARSET}'
 )
-
-_, test_async_db_session = create_engine_and_session(TEST_SQLALCHEMY_DATABASE_URL)
 
 
 async def override_get_db() -> AsyncSession:
