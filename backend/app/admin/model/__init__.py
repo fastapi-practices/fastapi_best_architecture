024beec9e3fa57ad607ee76fc5fa922787268e85
Wu Clan <jianhengwu0407@gmail.com>
2024-11-20 11:44:37 +0800

    Update the model file naming

--- backend/app/admin/model/__init__.py
+++ backend/app/admin/model/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from backend.app.admin.model.sys_api import Api
-from backend.app.admin.model.sys_casbin_rule import CasbinRule
-from backend.app.admin.model.sys_config import Config
-from backend.app.admin.model.sys_dept import Dept
-from backend.app.admin.model.sys_dict_data import DictData
-from backend.app.admin.model.sys_dict_type import DictType
-from backend.app.admin.model.sys_login_log import LoginLog
-from backend.app.admin.model.sys_menu import Menu
-from backend.app.admin.model.sys_opera_log import OperaLog
-from backend.app.admin.model.sys_role import Role
-from backend.app.admin.model.sys_user import User
-from backend.app.admin.model.sys_user_social import UserSocial
+from backend.app.admin.model.api import Api
+from backend.app.admin.model.casbin_rule import CasbinRule
+from backend.app.admin.model.config import Config
+from backend.app.admin.model.dept import Dept
+from backend.app.admin.model.dict_data import DictData
+from backend.app.admin.model.dict_type import DictType
+from backend.app.admin.model.login_log import LoginLog
+from backend.app.admin.model.menu import Menu
+from backend.app.admin.model.opera_log import OperaLog
+from backend.app.admin.model.role import Role
+from backend.app.admin.model.user import User
+from backend.app.admin.model.user_social import UserSocial
