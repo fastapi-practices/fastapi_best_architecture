f314aaf6bad90d305ba36e9963b79b291e63849b
Wu Clan <jianhengwu0407@gmail.com>
2025-01-10 13:56:43 +0800

    Update the route version define location

--- backend/app/admin/api/router.py
+++ backend/app/admin/api/router.py
@@ -7,8 +7,9 @@ from backend.app.admin.api.v1.log import router as log_router
 from backend.app.admin.api.v1.monitor import router as monitor_router
 from backend.app.admin.api.v1.oauth2 import router as oauth2_router
 from backend.app.admin.api.v1.sys import router as sys_router
+from backend.core.conf import settings
 
-v1 = APIRouter()
+v1 = APIRouter(prefix=settings.FASTAPI_API_V1_PATH)
 
 v1.include_router(auth_router)
 v1.include_router(oauth2_router)
