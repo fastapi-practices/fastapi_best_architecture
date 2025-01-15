a40d49f003d0bb268956d7bb8b0ff27d91d6976b
qhp13654398483 <w935415150@hotmail.com>
2025-01-15 05:28:57 +0000

    添加respons_model统一返回

--- backend/common/response/response_schema.py
+++ backend/common/response/response_schema.py
@@ -107,4 +107,56 @@ class ResponseBase:
         return MsgSpecJSONResponse({'code': res.code, 'msg': res.msg, 'data': data})
 
 
+# 动态创建模型的辅助函数
+def create_detail_response_model(data_model: Type[BaseModel]) -> Type[BaseModel]:
+    # 使用 Union 允许 data 字段既可以是单个实例也可以是列表
+    data_field_type = Optional[Union[data_model, List[data_model]]]
+
+    return create_model(
+        'DynamicCombinedModel',  # 新模型的名字
+        __base__=ResponseModel,  # 继承自 ResponseModel
+        data=(data_field_type, None)  # 更新 data 字段类型为给定的 data_model 或其列表
+    )
+
+
+# 定义链接信息模型
+class Links(BaseModel):
+    first: str= Field(..., description='首页链接')
+    last: str = Field(..., description="尾页链接")
+    self: str= Field(None, description="当前页链接")
+    next: str= Field(None, description="下一页链接")
+    prev: str|None= Field(None, description="上一页链接")
+
+
+# 定义分页信息模型
+class Pagination(BaseModel):
+    total: int = Field(..., description="总条数")
+    page: int = Field(..., description="当前页")
+    size: int = Field(..., description="每页数量")
+    total_pages: int = Field(..., description="总页数")
+
+
+# 动态创建组合模型的辅助函数
+def create_paged_response_model(item_model: Type[BaseModel]) -> Type[BaseModel]:
+    # 创建内部 data 模型
+    DataModel = create_model(
+        'DataModel',
+        items=(List[item_model], Field(default_factory=list, description="业务信息")),  # 列表，默认为空
+        total=(int, Field(..., description="总条数")),
+        page=(int, Field(..., description="当前页")),
+        size=(int, Field(..., description="每页数量")),
+        total_pages=(int, Field(..., description="总页数")),
+        links=(Links, Field(..., description="跳转链接")),
+    )
+
+    # 创建最终的 end 模型
+    EndModel = create_model(
+        'EndModel',
+        __base__=ResponseModel,  # 继承自 ResponseModel
+        data=(DataModel, Field(..., description="数据详情"))  # 添加 data 字段，类型为 DataModel
+    )
+
+    return EndModel
+
+
 response_base: ResponseBase = ResponseBase()
