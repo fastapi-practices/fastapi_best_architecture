b4d23a6106c3e7f1b343094921b0ade2f203fb53
Wu Clan <jianhengwu0407@gmail.com>
2025-01-17 00:16:50 +0800

    Add response model include data schema

--- backend/common/response/response_schema.py
+++ backend/common/response/response_schema.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from typing import Any
+from typing import Any, Generic, TypeVar
 
 from fastapi import Response
 from pydantic import BaseModel, ConfigDict
@@ -10,14 +10,12 @@ from backend.common.response.response_code import CustomResponse, CustomResponse
 from backend.core.conf import settings
 from backend.utils.serializers import MsgSpecJSONResponse
 
-_ExcludeData = set[int | str] | dict[int | str, Any]
-
-__all__ = ['ResponseModel', 'response_base']
+SchemaT = TypeVar('SchemaT')
 
 
 class ResponseModel(BaseModel):
     """
-    统一返回模型
+    通用型统一返回模型，不包含 data 数据结构
 
     E.g. ::
 
@@ -45,6 +43,31 @@ class ResponseModel(BaseModel):
     data: Any | None = None
 
 
+class ResponseSchemaModel(ResponseModel, Generic[SchemaT]):
+    """
+    包含 data schema 的统一返回模型，适用于非分页接口
+
+    E.g. ::
+
+        @router.get('/test', response_model=ResponseSchemaModel[GetApiListDetails])
+        def test():
+            return ResponseSchemaModel[GetApiListDetails](data=GetApiListDetails(...))
+
+
+        @router.get('/test')
+        def test() -> ResponseSchemaModel[GetApiListDetails]:
+            return ResponseSchemaModel[GetApiListDetails](data=GetApiListDetails(...))
+
+
+        @router.get('/test')
+        def test() -> ResponseSchemaModel[GetApiListDetails]:
+            res = CustomResponseCode.HTTP_200
+            return ResponseSchemaModel[GetApiListDetails](code=res.code, msg=res.msg, data=GetApiListDetails(...))
+    """
+
+    data: SchemaT
+
+
 class ResponseBase:
     """
     统一返回方法
@@ -61,7 +84,9 @@ class ResponseBase:
     """
 
     @staticmethod
-    def __response(*, res: CustomResponseCode | CustomResponse = None, data: Any | None = None) -> ResponseModel:
+    def __response(
+        *, res: CustomResponseCode | CustomResponse = None, data: Any | None = None
+    ) -> ResponseModel | ResponseSchemaModel:
         """
         请求成功返回通用方法
 
@@ -76,7 +101,7 @@ class ResponseBase:
         *,
         res: CustomResponseCode | CustomResponse = CustomResponseCode.HTTP_200,
         data: Any | None = None,
-    ) -> ResponseModel:
+    ) -> ResponseModel | ResponseSchemaModel:
         return self.__response(res=res, data=data)
 
     def fail(
@@ -84,7 +109,7 @@ class ResponseBase:
         *,
         res: CustomResponseCode | CustomResponse = CustomResponseCode.HTTP_400,
         data: Any = None,
-    ) -> ResponseModel:
+    ) -> ResponseModel | ResponseSchemaModel:
         return self.__response(res=res, data=data)
 
     @staticmethod
@@ -92,7 +117,7 @@ class ResponseBase:
         *,
         res: CustomResponseCode | CustomResponse = CustomResponseCode.HTTP_200,
         data: Any | None = None,
-    ) -> Response:
+    ) -> Response | ResponseSchemaModel:
         """
         此方法是为了提高接口响应速度而创建的，如果返回数据无需进行 pydantic 解析和验证，则推荐使用，相反，请不要使用！
 
