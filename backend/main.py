b108410a2612726b7e58a86006a2e37e8c45b308
huyuwei1996 <huyuwei1996@gmail.com>
2025-02-17 11:44:33 +0800

    fix: uvicron.Server().run cannot reload

--- backend/main.py
+++ backend/main.py
@@ -13,8 +13,6 @@ if __name__ == '__main__':
     # 如果你喜欢在 IDE 中进行 DEBUG，main 启动方法会很有帮助
     # 如果你喜欢通过 print 方式进行调试，建议使用 fastapi cli 方式启动服务
     try:
-        config = uvicorn.Config(app=f'{Path(__file__).stem}:app', reload=True)
-        server = uvicorn.Server(config)
-        server.run()
+        uvicorn.run(app=f'{Path(__file__).stem}:app', reload=True)
     except Exception as e:
         raise e
