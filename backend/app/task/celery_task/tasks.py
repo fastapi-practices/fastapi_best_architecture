3763804ac95202d770ccc2c0da4335eba578de0e
Wu Clan <jianhengwu0407@gmail.com>
2025-03-22 00:04:18 +0800

    Optimize task

--- backend/app/task/celery_task/tasks.py
+++ backend/app/task/celery_task/tasks.py
@@ -7,5 +7,6 @@ from backend.app.task.celery import celery_app
 
 @celery_app.task(name='task_demo_async')
 async def task_demo_async() -> str:
+    """异步示例任务，模拟耗时操作"""
     await sleep(20)
     return 'test async'
