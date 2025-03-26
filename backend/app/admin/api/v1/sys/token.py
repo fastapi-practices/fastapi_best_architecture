9f0530f813db264388c1136a46f772b63f602fd7
Wu Clan <jianhengwu0407@gmail.com>
2025-03-27 00:35:38 +0800

    Optimize plugin、token api

--- backend/app/admin/api/v1/sys/token.py
+++ backend/app/admin/api/v1/sys/token.py
@@ -21,8 +21,24 @@ router = APIRouter()
 @router.get('', summary='获取令牌列表', dependencies=[DependsJwtAuth])
 async def get_tokens(username: Annotated[str | None, Query()] = None) -> ResponseSchemaModel[list[GetTokenDetail]]:
     token_keys = await redis_client.keys(f'{settings.TOKEN_REDIS_PREFIX}:*')
-    token_online = await redis_client.smembers(settings.TOKEN_ONLINE_REDIS_PREFIX)
-    data = []
+    online_clients = await redis_client.smembers(settings.TOKEN_ONLINE_REDIS_PREFIX)
+    data: list[GetTokenDetail] = []
+
+    def append_token_detail() -> None:
+        data.append(
+            token_detail.model_copy(
+                update={
+                    'username': extra_info.get('username', '未知'),
+                    'nickname': extra_info.get('nickname', '未知'),
+                    'ip': extra_info.get('ip', '未知'),
+                    'os': extra_info.get('os', '未知'),
+                    'browser': extra_info.get('browser', '未知'),
+                    'device': extra_info.get('device', '未知'),
+                    'last_login_time': extra_info.get('last_login_time', '未知'),
+                }
+            )
+        )
+
     for key in token_keys:
         token = await redis_client.get(key)
         token_payload = jwt_decode(token)
@@ -36,31 +52,15 @@ async def get_tokens(username: Annotated[str | None, Query()] = None) -> Respons
             os='未知',
             browser='未知',
             device='未知',
-            status=StatusType.disable if session_uuid not in token_online else StatusType.enable,
+            status=StatusType.enable if session_uuid in online_clients else StatusType.disable,
             last_login_time='未知',
             expire_time=token_payload.expire_time,
         )
         extra_info = await redis_client.get(f'{settings.TOKEN_EXTRA_INFO_REDIS_PREFIX}:{session_uuid}')
         if extra_info:
-
-            def append_token_detail():
-                data.append(
-                    token_detail.model_copy(
-                        update={
-                            'username': extra_info.get('username'),
-                            'nickname': extra_info.get('nickname'),
-                            'ip': extra_info.get('ip'),
-                            'os': extra_info.get('os'),
-                            'browser': extra_info.get('browser'),
-                            'device': extra_info.get('device'),
-                            'last_login_time': extra_info.get('last_login_time'),
-                        }
-                    )
-                )
-
             extra_info = json.loads(extra_info)
             if extra_info.get('login_type') != 'swagger':
-                if username:
+                if username is not None:
                     if username == extra_info.get('username'):
                         append_token_detail()
                 else:
@@ -79,8 +79,8 @@ async def get_tokens(username: Annotated[str | None, Query()] = None) -> Respons
     ],
 )
 async def kick_out(
-    request: Request, pk: Annotated[int, Path(..., description='用户 ID')], session_uuid: KickOutToken
+    request: Request, pk: Annotated[int, Path(..., description='用户 ID')], obj: KickOutToken
 ) -> ResponseModel:
     superuser_verify(request)
-    await revoke_token(pk)
+    await revoke_token(str(pk), obj.session_uuid)
     return response_base.success()
