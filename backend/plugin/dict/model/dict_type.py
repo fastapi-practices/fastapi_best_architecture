1cc020efdd14aa1e410b8ccfbd20f28d7a7fb8cc
Wu Clan <jianhengwu0407@gmail.com>
2025-04-17 11:06:29 +0800

    Update unique columns in dict models (#577)

--- backend/plugin/dict/model/dict_type.py
+++ backend/plugin/dict/model/dict_type.py
@@ -21,7 +21,7 @@ class DictType(Base):
     __tablename__ = 'sys_dict_type'
 
     id: Mapped[id_key] = mapped_column(init=False)
-    name: Mapped[str] = mapped_column(String(32), unique=True, comment='字典类型名称')
+    name: Mapped[str] = mapped_column(String(32), comment='字典类型名称')
     code: Mapped[str] = mapped_column(String(32), unique=True, comment='字典类型编码')
     status: Mapped[int] = mapped_column(default=1, comment='状态（0停用 1正常）')
     remark: Mapped[str | None] = mapped_column(
