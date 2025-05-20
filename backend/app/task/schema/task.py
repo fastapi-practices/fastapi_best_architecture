f1b23dcf744bcd91e2aa405f9658ed1b963599a9
Wu Clan <jianhengwu0407@gmail.com>
2025-05-20 15:08:35 +0800

    Fix the task result schema param type

--- backend/app/task/schema/task.py
+++ backend/app/task/schema/task.py
@@ -19,11 +19,11 @@ class TaskResult(SchemaBase):
     """任务执行结果"""
 
     result: str = Field(description='任务执行结果')
-    traceback: str = Field(description='错误堆栈信息')
+    traceback: str | None = Field(None, description='错误堆栈信息')
     status: str = Field(description='任务状态')
-    name: str = Field(description='任务名称')
+    name: str | None = Field(None, description='任务名称')
     args: list[Any] | None = Field(None, description='任务函数位置参数')
     kwargs: dict[str, Any] | None = Field(None, description='任务函数关键字参数')
-    worker: str = Field(description='执行任务的 worker')
+    worker: str | None = Field(None, description='执行任务的 worker')
     retries: int | None = Field(None, description='重试次数')
     queue: str | None = Field(None, description='任务队列')
