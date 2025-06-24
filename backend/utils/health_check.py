6e8baac4ee492a4e8e98ac749506d52f3bbf0a80
Wu Clan <jianhengwu0407@gmail.com>
2025-06-24 10:39:58 +0800

    Simplify custom response status codes

--- backend/utils/health_check.py
+++ backend/utils/health_check.py
@@ -12,6 +12,7 @@ from fastapi.routing import APIRoute
 
 from backend.common.exception import errors
 from backend.common.log import log
+from backend.common.response.response_code import StandardResponseCode
 
 
 def ensure_unique_route_names(app: FastAPI) -> None:
@@ -39,7 +40,9 @@ async def http_limit_callback(request: Request, response: Response, expire: int)
     :return:
     """
     expires = ceil(expire / 1000)
-    raise errors.HTTPError(code=429, msg='请求过于频繁，请稍后重试', headers={'Retry-After': str(expires)})
+    raise errors.HTTPError(
+        code=StandardResponseCode.HTTP_429, msg='请求过于频繁，请稍后重试', headers={'Retry-After': str(expires)}
+    )
 
 
 def timer(func) -> Callable:
