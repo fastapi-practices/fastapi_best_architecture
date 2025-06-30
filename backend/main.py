51aa46bad53d1c2bc27e7264f1fa548338bdad10
Wu Clan <jianhengwu0407@gmail.com>
2025-06-30 09:27:29 +0800

    Update main params

--- backend/main.py
+++ backend/main.py
@@ -8,22 +8,22 @@ from backend.core.registrar import register_app
 from backend.plugin.tools import get_plugins, install_requirements
 from backend.utils.timezone import timezone
 
-print_log_style = f'{timezone.to_str(timezone.now(), "%Y-%m-%d %H:%M:%S.%M0")} | fba      |  -  | '
-console.print(Text(f'{print_log_style}检测插件依赖...', style='bold cyan'))
+_print_log_style = f'{timezone.to_str(timezone.now(), "%Y-%m-%d %H:%M:%S.%M0")} | fba      |  -  | '
+console.print(Text(f'{_print_log_style}检测插件依赖...', style='bold cyan'))
 
-plugins = get_plugins()
+_plugins = get_plugins()
 
 with Progress(
-    SpinnerColumn(finished_text=f'[bold green]{print_log_style}插件准备就绪[/]'),
+    SpinnerColumn(finished_text=f'[bold green]{_print_log_style}插件准备就绪[/]'),
     TextColumn('[bold green]{task.completed}/{task.total}[/]'),
     TimeElapsedColumn(),
     console=console,
 ) as progress:
-    task = progress.add_task('安装插件依赖...', total=len(plugins))
-    for plugin in plugins:
+    task = progress.add_task('安装插件依赖...', total=len(_plugins))
+    for plugin in _plugins:
         install_requirements(plugin)
         progress.advance(task)
 
-console.print(Text(f'{print_log_style}启动服务...', style='bold magenta'))
+console.print(Text(f'{_print_log_style}启动服务...', style='bold magenta'))
 
 app = register_app()
