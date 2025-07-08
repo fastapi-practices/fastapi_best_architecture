5b7ebea38dfbfc5f5c243e873c48bfb04cb155be
Wu Clan <jianhengwu0407@gmail.com>
2025-07-08 16:52:00 +0800

    Update the celery conf

--- backend/app/task/celery.py
+++ backend/app/task/celery.py
@@ -19,31 +19,23 @@ def init_celery() -> celery.Celery:
 
     app = celery.Celery(
         'fba_celery',
-        enable_utc=False,
-        timezone=settings.DATETIME_TIMEZONE,
-        beat_schedule=LOCAL_BEAT_SCHEDULE,
-        broker_url=(
-            f'redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:'
-            f'{settings.REDIS_PORT}/{settings.CELERY_BROKER_REDIS_DATABASE}'
-        )
+        broker=f'redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.CELERY_BROKER_REDIS_DATABASE}'
         if settings.CELERY_BROKER == 'redis'
-        else (
-            f'amqp://{settings.CELERY_RABBITMQ_USERNAME}:{settings.CELERY_RABBITMQ_PASSWORD}@'
-            f'{settings.CELERY_RABBITMQ_HOST}:{settings.CELERY_RABBITMQ_PORT}'
-        ),
+        else f'amqp://{settings.CELERY_RABBITMQ_USERNAME}:{settings.CELERY_RABBITMQ_PASSWORD}@{settings.CELERY_RABBITMQ_HOST}:{settings.CELERY_RABBITMQ_PORT}',
         broker_connection_retry_on_startup=True,
-        result_backend=(
-            f'redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:'
-            f'{settings.REDIS_PORT}/{settings.CELERY_BACKEND_REDIS_DATABASE}'
-        ),
-        result_backend_transport_options={
-            'global_keyprefix': settings.CELERY_BACKEND_REDIS_PREFIX,
-            'retry_policy': {
-                'timeout': settings.CELERY_BACKEND_REDIS_TIMEOUT,
-            },
-        },
+        backend=f'db+{settings.DATABASE_TYPE + "+pymysql" if settings.DATABASE_TYPE == "mysql" else settings.DATABASE_TYPE}'  # noqa: E501
+        f'://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_SCHEMA}',
+        database_engine_options={'echo': settings.DATABASE_ECHO},
+        result_extended=True,
+        result_expires=0,
+        beat_schedule=LOCAL_BEAT_SCHEDULE,
+        beat_scheduler='app.task.utils.schedulers:DatabaseScheduler',
         task_cls='app.task.tasks.base:TaskBase',
         task_track_started=True,
+        enable_utc=False,
+        timezone=settings.DATETIME_TIMEZONE,
+        # TODO: Update this work if celery version >= 6.0.0
+        worker_pool=celery_aio_pool.pool.AsyncIOPool,
     )
 
     # 自动发现任务
