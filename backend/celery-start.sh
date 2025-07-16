65f3d6ad7924caeffc7a5d03031d04231e57f680
Wu Clan <jianhengwu0407@gmail.com>
2025-07-16 12:28:57 +0800

    Add CLI support for startup celery services (#724)
    * Add CLI support for startup celery services
    
    * update the cli file

--- backend/celery-start.sh
+++ backend/celery-start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # work && beat
-celery -A app.task.celery worker -l info -P gevent -c 100 &
+celery -A backend.app.task.celery worker -l info -P gevent -c 100 &
 
 # beat
-celery -A app.task.celery beat -l info &
+celery -A backend.app.task.celery beat -l info &
 
 # flower
-celery -A app.task.celery flower --port=8555 --basic-auth=admin:123456
+celery -A backend.app.task.celery flower --port=8555 --basic-auth=admin:123456
