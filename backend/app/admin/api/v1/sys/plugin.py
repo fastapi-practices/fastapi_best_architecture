d01e04dc83660cf62a2e922d9c1390531d56fa90
Wu Clan <jianhengwu0407@gmail.com>
2025-08-14 22:39:00 +0800

    Fix lint

--- backend/app/admin/api/v1/sys/plugin.py
+++ backend/app/admin/api/v1/sys/plugin.py
@@ -45,9 +45,7 @@ async def install_plugin(
     repo_url: Annotated[str | None, Query(description='插件 git 仓库地址')] = None,
 ) -> ResponseModel:
     plugin = await plugin_service.install(type=type, file=file, repo_url=repo_url)
-    return response_base.success(
-        res=CustomResponse(code=200, msg=t('success.plugin.install_success', plugin=plugin))
-    )
+    return response_base.success(res=CustomResponse(code=200, msg=t('success.plugin.install_success', plugin=plugin)))
 
 
 @router.delete(
@@ -61,9 +59,7 @@ async def install_plugin(
 )
 async def uninstall_plugin(plugin: Annotated[str, Path(description='插件名称')]) -> ResponseModel:
     await plugin_service.uninstall(plugin=plugin)
-    return response_base.success(
-        res=CustomResponse(code=200, msg=t('success.plugin.uninstall_success', plugin=plugin))
-    )
+    return response_base.success(res=CustomResponse(code=200, msg=t('success.plugin.uninstall_success', plugin=plugin)))
 
 
 @router.put(
