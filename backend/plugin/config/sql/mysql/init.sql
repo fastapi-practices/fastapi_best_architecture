d929f0dbdc040e28ea00621d9fa4ab30eb0c4dad
Wu Clan <jianhengwu0407@gmail.com>
2025-09-03 19:57:11 +0800

    Fix sql script syntax error in config plugin (#793)

--- backend/plugin/config/sql/mysql/init.sql
+++ backend/plugin/config/sql/mysql/init.sql
@@ -1,8 +1,8 @@
-insert into sys_config (id, name, type, key, value, is_frontend, remark, created_time, updated_time)
+insert into sys_config (id, name, type, `key`, value, is_frontend, remark, created_time, updated_time)
 values
-(1, '状态', 'EMAIL', 'EMAIL_STATUS', '1', 0, null, '2025-08-17 23:08:44', '2025-08-19 10:53:24'),
-(2, '服务器地址', 'EMAIL', 'EMAIL_HOST', 'smtp.qq.com', 0, null, '2025-08-17 23:08:57', '2025-08-19 10:53:24'),
-(3, '服务器端口', 'EMAIL', 'EMAIL_PORT', '465', 0, null, '2025-08-17 23:10:53', '2025-08-19 10:53:24'),
-(4, '邮箱账号', 'EMAIL', 'EMAIL_USERNAME', 'fba@qq.com', 0, null, '2025-08-17 23:11:47', '2025-08-19 10:53:24'),
-(5, '邮箱密码', 'EMAIL', 'EMAIL_PASSWORD', '', 0, null, '2025-08-17 23:12:09', '2025-08-19 10:53:24'),
-(6, 'SSL 加密', 'EMAIL', 'EMAIL_SSL', '1', 0, null, '2025-08-17 23:52:46', '2025-08-19 10:53:24');
+(1, '状态', 'EMAIL', 'EMAIL_STATUS', '1', 0, null, now(), null),
+(2, '服务器地址', 'EMAIL', 'EMAIL_HOST', 'smtp.qq.com', 0, null, now(), null),
+(3, '服务器端口', 'EMAIL', 'EMAIL_PORT', '465', 0, null, now(), null),
+(4, '邮箱账号', 'EMAIL', 'EMAIL_USERNAME', 'fba@qq.com', 0, null, now(), null),
+(5, '邮箱密码', 'EMAIL', 'EMAIL_PASSWORD', '', 0, null, now(), null),
+(6, 'SSL 加密', 'EMAIL', 'EMAIL_SSL', '1', 0, null, now(), null),
