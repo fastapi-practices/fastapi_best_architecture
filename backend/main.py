b20a2960e9de4cb9811c214196f30ac1b22c1f6a
Wu Clan <jianhengwu0407@gmail.com>
2025-09-07 19:25:51 +0800

    Fix console import in main module

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,9 @@
 from rich.progress import Progress, SpinnerColumn, TextColumn, TimeElapsedColumn
 from rich.text import Text
 
-from backend import console
 from backend.core.registrar import register_app
 from backend.plugin.tools import get_plugins, install_requirements
+from backend.utils.console import console
 from backend.utils.timezone import timezone
 
 _log_prefix = f'{timezone.to_str(timezone.now(), "%Y-%m-%d %H:%M:%S.%M0")} | {"INFO": <8} | - | '
