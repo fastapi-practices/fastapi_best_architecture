02b42157425c735ac41d3a2b5019a030af9568f5
Wu Clan <jianhengwu0407@gmail.com>
2025-10-18 15:49:55 +0800

    Fix tasks related to deleting db logs (#863)

--- backend/app/task/tasks/db_log/tasks.py
+++ backend/app/task/tasks/db_log/tasks.py
@@ -2,17 +2,20 @@ from celery import shared_task
 
 from backend.app.admin.service.login_log_service import login_log_service
 from backend.app.admin.service.opera_log_service import opera_log_service
+from backend.database.db import async_db_session
 
 
 @shared_task
 async def delete_db_opera_log() -> str:
     """自动删除数据库操作日志"""
-    await opera_log_service.delete_all()
-    return 'Success'
+    async with async_db_session.begin() as db:
+        await opera_log_service.delete_all(db=db)
+        return 'Success'
 
 
 @shared_task
 async def delete_db_login_log() -> str:
     """自动删除数据库登录日志"""
-    await login_log_service.delete_all()
-    return 'Success'
+    async with async_db_session.begin() as db:
+        await login_log_service.delete_all(db=db)
+        return 'Success'
