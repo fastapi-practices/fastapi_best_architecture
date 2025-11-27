ed4e312cb143756ab3fdc0c632e90fd6455c2ca7
Wu Clan <jianhengwu0407@gmail.com>
2025-11-27 19:20:44 +0800

    Update code generation interface definitions (#939)
    * Update code generation interface definitions
    
    * Update more
    
    * Resume irrelevant updates

--- backend/plugin/code_generator/api/router.py
+++ backend/plugin/code_generator/api/router.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter
 
 from backend.core.conf import settings
 from backend.plugin.code_generator.api.v1.business import router as business_router
-from backend.plugin.code_generator.api.v1.code import router as gen_router
 from backend.plugin.code_generator.api.v1.column import router as column_router
+from backend.plugin.code_generator.api.v1.gen import router as gen_router
 
-v1 = APIRouter(prefix=f'{settings.FASTAPI_API_V1_PATH}/generates', tags=['代码生成'])
+v1 = APIRouter(prefix=f'{settings.FASTAPI_API_V1_PATH}/code-generation', tags=['代码生成'])
 
 v1.include_router(business_router, prefix='/businesses')
 v1.include_router(column_router, prefix='/columns')
-v1.include_router(gen_router, prefix='/codes')
+v1.include_router(gen_router, prefix='/generations')
