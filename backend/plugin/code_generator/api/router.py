8cce6a094f83c3ddb61c0a78bf2d9c1e364a9df8
Wu Clan <jianhengwu0407@gmail.com>
2025-11-27 18:05:51 +0800

    Update code generation interface definitions

--- backend/plugin/code_generator/api/router.py
+++ backend/plugin/code_generator/api/router.py
@@ -5,7 +5,7 @@ from backend.plugin.code_generator.api.v1.business import router as business_rou
 from backend.plugin.code_generator.api.v1.code import router as gen_router
 from backend.plugin.code_generator.api.v1.column import router as column_router
 
-v1 = APIRouter(prefix=f'{settings.FASTAPI_API_V1_PATH}/generates', tags=['代码生成'])
+v1 = APIRouter(prefix=f'{settings.FASTAPI_API_V1_PATH}/code-generation', tags=['代码生成'])
 
 v1.include_router(business_router, prefix='/businesses')
 v1.include_router(column_router, prefix='/columns')
