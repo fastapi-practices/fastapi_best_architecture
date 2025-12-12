c828661d6136614c881da448e49a3f6f8ebd2517
Wu Clan <jianhengwu0407@gmail.com>
2025-12-12 13:58:27 +0800

    Fix some configs and loguru integration

--- backend/utils/otel.py
+++ backend/utils/otel.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
+from loguru import logger
 from opentelemetry import trace
 from opentelemetry._logs import set_logger_provider
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry.sdk._logs import LoggerProvider
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs._internal.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from backend import __version__
+from backend.common.log import request_id_filter
 from backend.core.conf import settings
 
 
@@ -43,5 +45,14 @@ def init_otel(app: FastAPI) -> None:
     log_exporter = OTLPLogExporter(endpoint='http://fba_alloy:4317', insecure=True)
     logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
 
-    LoggingInstrumentor().instrument(set_logging_format=False)
+    LoggingInstrumentor().instrument(set_logging_format=True)
     FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
+
+    # 为 loguru 添加 OpenTelemetry handler
+    otel_handler = LoggingHandler(logger_provider=logger_provider)
+    logger.add(
+        otel_handler,
+        level=settings.LOG_STD_LEVEL,
+        format=settings.LOG_FORMAT,
+        filter=lambda record: request_id_filter(record),
+    )
