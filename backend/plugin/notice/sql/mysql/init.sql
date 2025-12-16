6a88f1608f637c624a24f1b6b8c664e5539ad08b
Wu Clan <jianhengwu0407@gmail.com>
2025-12-16 16:52:58 +0800

    Add code generation and notice SQL scripts

---
+++ backend/plugin/notice/sql/mysql/init.sql
@@ -0,0 +1,18 @@
+insert into sys_notice (id, title, type, status, content, created_time, updated_time)
+values (1, 'hahahahahaahahaha', 0, 1, '你好😄
+
+```
+print(''fba yyds'')
+```
+
+⚡⚡⚡
+
+| col1 | col2 | col3 |
+| ---- | ---- | ---- |
+|      |      |      |
+|      |      |      |
+
+* 1
+* 2
+* 3
+', '2025-12-15 15:33:16', null);
