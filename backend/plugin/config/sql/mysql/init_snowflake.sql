c964c81bb460123824a36fc16faebf830cf82596
Wu Clan <jianhengwu0407@gmail.com>
2026-01-06 14:29:28 +0800

    Fix SQL scripts error in config plugin

--- backend/plugin/config/sql/mysql/init_snowflake.sql
+++ backend/plugin/config/sql/mysql/init_snowflake.sql
@@ -1,6 +1,6 @@
 insert into sys_config (id, name, type, `key`, value, is_frontend, remark, created_time, updated_time)
 values
-(2069061886627938304, '状态', 'EMAIL', 'EMAIL_STATUS', '1', false, null, now(), null),
+(2069061886627938304, '状态', 'EMAIL', 'EMAIL_CONFIG_STATUS', '1', false, null, now(), null),
 (2069061886627938305, '服务器地址', 'EMAIL', 'EMAIL_HOST', 'smtp.qq.com', false, null, now(), null),
 (2069061886627938306, '服务器端口', 'EMAIL', 'EMAIL_PORT', '465', false, null, now(), null),
 (2069061886627938307, '邮箱账号', 'EMAIL', 'EMAIL_USERNAME', 'fba@qq.com', false, null, now(), null),
--- backend/plugin/config/sql/postgresql/init.sql
+++ backend/plugin/config/sql/postgresql/init.sql
@@ -1,6 +1,6 @@
 insert into sys_config (id, name, type, "key", value, is_frontend, remark, created_time, updated_time)
 values
-(1, '状态', 'EMAIL', 'EMAIL_STATUS', '1', false, null, now(), null),
+(1, '状态', 'EMAIL', 'EMAIL_CONFIG_STATUS', '1', false, null, now(), null),
 (2, '服务器地址', 'EMAIL', 'EMAIL_HOST', 'smtp.qq.com', false, null, now(), null),
 (3, '服务器端口', 'EMAIL', 'EMAIL_PORT', '465', false, null, now(), null),
 (4, '邮箱账号', 'EMAIL', 'EMAIL_USERNAME', 'fba@qq.com', false, null, now(), null),
