b0ee6952ad07f8c85218d48e5e9c70320df32d04
Wu Clan <jianhengwu0407@gmail.com>
2026-02-06 14:04:49 +0800

    Fix the login config location

--- backend/app/admin/service/auth_service.py
+++ backend/app/admin/service/auth_service.py
@@ -99,6 +99,7 @@ class AuthService:
         """
         user = None
         try:
+            await load_login_config(db)
             if settings.LOGIN_CAPTCHA_ENABLED:
                 if not obj.uuid or not obj.captcha:
                     raise errors.RequestError(msg=t('error.captcha.invalid'))
@@ -110,9 +111,6 @@ class AuthService:
                 await redis_client.delete(f'{settings.LOGIN_CAPTCHA_REDIS_PREFIX}:{obj.uuid}')
 
             user, days_remaining = await self.user_verify(db, obj.username, obj.password)
-
-            await load_login_config(db)
-
             await user_dao.update_login_time(db, obj.username)
             await db.refresh(user)
             access_token_data = await create_access_token(
