0bd41a9e67c3af96077b20b403bf93c1824f3e1d
Wu Clan <jianhengwu0407@gmail.com>
2026-03-05 18:36:44 +0800

    Optimize the CLI command console output style (#1094)
    * Optimize the CLI command console output style
    
    * Update output functions
    
    * Update destroy sql style

--- backend/utils/console.py
+++ backend/utils/console.py
@@ -1,3 +1,28 @@
-from rich import get_console
+from rich.console import Console
 
-console = get_console()
+
+class CustomConsole(Console):
+    """自定义控制台"""
+
+    def note(self, msg: str) -> None:
+        """输出注释"""
+        self.print(f'[bold white]•[/] [white]{msg}[/]')
+
+    def info(self, msg: str) -> None:
+        """输出信息"""
+        self.print(f'[bold cyan]•[/] {msg}')
+
+    def tip(self, msg: str) -> None:
+        """输出提示消息"""
+        self.print(f'[bold green]✓[/] [green]{msg}[/]')
+
+    def warning(self, msg: str) -> None:
+        """输出警告消息"""
+        self.print(f'[bold yellow]⚠[/] [yellow]{msg}[/]')
+
+    def caution(self, msg: str) -> None:
+        """输出危险消息"""
+        self.print(f'[bold red]✗[/] [red]{msg}[/]')
+
+
+console = CustomConsole()
