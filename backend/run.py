4abe09dbc375a2f5e44201ad2bb3fa2cdbbe2e44
Wu Clan <jianhengwu0407@gmail.com>
2026-03-09 21:59:00 +0800

    Replace the uvicorn runner with granian (#1105)

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,8 @@
-import os
+import granian
 
-import uvicorn
+from backend.cli import CustomReloadFilter
 
 if __name__ == '__main__':
-    # 为什么独立此启动文件：https://stackoverflow.com/questions/64003384
-
     # DEBUG:
     # 如果你喜欢在 IDE 中进行 DEBUG，可在 IDE 中直接右键启动此文件
     # 如果你喜欢通过 print 方式进行调试，建议使用 fba cli 方式启动服务
@@ -13,10 +11,11 @@ if __name__ == '__main__':
     # 如果你正在通过 python 命令启动此文件，请遵循以下事宜：
     # 1. 按照官方文档通过 uv 安装依赖
     # 2. 命令行空间位于 backend 目录下
-    uvicorn.run(
-        app='backend.main:app',
-        host='127.0.0.1',
+    granian.Granian(
+        target='main:app',
+        interface='asgi',
+        address='127.0.0.1',
         port=8000,
         reload=True,
-        reload_excludes=[os.path.abspath('../.venv')],
-    )
+        reload_filter=CustomReloadFilter,
+    ).serve()
